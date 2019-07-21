2e11c1c6ec18c0780091956f04e9d81d97b9b902
n3rsti <krzysiek.witucki55@gmail.com>
2019-07-21 13:45:53 +0200

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.onload = function(){
     document.querySelector(".random").addEventListener("click", numbersData)
 };
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("'./sw.js'")
+    navigator.serviceWorker.register('./sw.js')
         .then((reg) => console.log("Service worker registered", reg))
         .catch((err) => console.log('Service worker not registered', err));
 };
